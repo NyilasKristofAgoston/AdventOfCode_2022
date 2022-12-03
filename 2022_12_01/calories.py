dc9ef426e082185f35f249f3f5fb705b594adccb
NyilasKristofAgoston <53142540+NyilasKristofAgoston@users.noreply.github.com>
2022-12-03 22:00:52 +0100

    1

--- 2022_12_01/calories.py
+++ 2022_12_01/calories.py
@@ -1,1 +1,19 @@
-print("I am ironman")
+f = open("C:\input.txt", 'r')
+
+index = 0
+
+osszegLista = []
+szamlalo = 0
+
+for i in f:
+
+    if i == "\n":
+        osszegLista.append(szamlalo)
+        index += 1
+        szamlalo = 0
+    else:
+        szamlalo += int(i)
+
+print(max(osszegLista))
+
+
